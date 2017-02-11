1101f8794980bda438dfce476aed767367012ec6
Saurabh Modh <saurabh.modh@hashedin.com>
2017-02-11 23:27:15 +0530

    Props

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import React from 'react';
 
 class App extends React.Component {
   render (){
+    let txt = this.props.txt
     return (
       <div>
         <h1>Hello</h1>
         <h2>World</h2>
+        <h3>{txt} {this.props.num}</h3>
       </div>
     )
     // return <h1 className="" >Hello World:</h1>
     // return React.createElement ('h1',null,'Hello World!!')
   }
+}
+
+App.propTypes = {
+  txt : React.PropTypes.string,
+  num : React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "Hi",
+  num: 5
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <App txt="Hi Saurabh" />,
   document.getElementById('root')
 );
