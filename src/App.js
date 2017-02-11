ba5f489c75d5080a8e52ef36f5943f2ca1d79832
Saurabh Modh <saurabh.modh@hashedin.com>
2017-02-11 23:10:07 +0530

    Creating components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App = () => <h1>Hello World!</h1>
+// // stateless component
+// const App = () => <h1>Hello World!</h1>
+
+class App extends React.Component {
+  render (){
+    return <h1 className="" >Hello World:</h1>
+    // return React.createElement ('h1',null,'Hello World!!')
+  }
+}
 
 export default App
