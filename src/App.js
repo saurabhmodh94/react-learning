855a557c55ce88ccd3921f6b0cb5ca9e879e6a6f
Saurabh Modh <saurabh.modh@hashedin.com>
2017-02-13 23:06:58 +0530

    code clean

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-// // stateless component
-// const App = () => <h1>Hello World!</h1>
-
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      txt: "Hello World",
-      num: 0
+      txt: "Hello World"
     }
   }
   update (e){
@@ -20,32 +16,10 @@ class App extends React.Component {
     return (
       <div>
         <input type="text" onChange={this.update.bind(this)}></input>
-        <h3>{this.state.txt} - {this.state.num}</h3>
+        <h3>{this.state.txt}</h3>
       </div>
     )
-    // let txt = this.props.txt
-
-    // return (
-    //   <div>
-    //     <input type="text" onChange={this.update.bind(this)}></input>
-    //     <h1>Hello</h1>
-    //     <h2>World</h2>
-    //     <h3>{txt} {this.props.num}</h3>
-    //   </div>
-    // )
-    // return <h1 className="" >Hello World:</h1>
-    // return React.createElement ('h1',null,'Hello World!!')
   }
-}
-
-App.propTypes = {
-  txt : React.PropTypes.string,
-  num : React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "Hi",
-  num: 5
 }
 
 export default App
