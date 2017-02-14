daf82d9636b63d63103d13458fe7ea15a0461d95
Saurabh Modh <saurabh.modh@hashedin.com>
2017-02-14 18:48:02 +0530

    refs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor(){
+  constructor (){
     super();
     this.state = {
-      event : "--"
+      a: "",
+      b: "",
+      c: "",
+      d: ""
     }
   }
   update(e){
     this.setState({
-        event: e.type
+      a : this.a.value,
+      b : this.refs.b.value,
+      c : ReactDOM.findDOMNode(this.c).value,
+      d : this.d.refs.compo.value,
     })
   }
   render(){
     return (
       <div>
-        <textarea onFocus={this.update.bind(this)}
-        onClick={this.update.bind(this)}
-        onCut={this.update.bind(this)}>
-        Click
-        </textarea>
-        <h1>{this.state.event}</h1>
+        <input type="text" ref={node => this.a = node} onChange={this.update.bind(this)}></input>{this.state.a}
+        <hr />
+        <input type="text" ref="b" onChange={this.update.bind(this)}></input>{this.state.b}
+        <hr />
+        {
+          //comment
+        }
+        <Input upd={this.update.bind(this)} ref={ component => this.c = component} />{this.state.c}
+        <hr />
+        <InputDiv upd1={this.update.bind(this)} ref={ component => this.d = component} />{this.state.d}
       </div>
     )
+  }
+}
+
+class Input extends React.Component {
+  render () {
+    return <input type="text" onChange={this.props.upd}></input>
+  }
+}
+
+class InputDiv extends React.Component {
+  render () {
+    return <div>
+      <input type="text" ref="compo" onChange={this.props.upd1}></input>
+    </div>
+
   }
 }
 
