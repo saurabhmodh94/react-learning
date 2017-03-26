e0e7974dcd762496abed7d659bf0a3e13b558998
saurabhmodh94 <saurabhmodh94@gmail.com>
2017-03-26 17:14:27 +0530

    reactchildren-utilities

--- src/App.js
+++ src/App.js
@@ -3,45 +3,28 @@ import ReactDOM from 'react-dom';
 import './App.css';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      input:'/*Write your code.*/',
-      output: '',
-      error: ''
-    }
-  }
-
-  update(event){
-    let code =event.target.value;
-    try {
-      this.setState({
-        output: window.Babel.transform(code, { presets: ['es2015','react']}).code,
-        error: ''
-      })
-    } catch (err) {
-      this.setState({
-        error:err.message
-      })
-    }
-  }
-
   render () {
     return(
-      <div>
-        <p>
-          {this.state.error}
-        </p>
-        <textarea
-          onChange={this.update.bind(this)}
-          defaultValue={this.state.input}>
-        </textarea>
-        <pre>
-          {this.state.output}
-        </pre>
-      </div>
+      <Parent>
+        <div className="classA">
+        </div>
+        <div className="classB">
+        </div>
+      </Parent>
     )
   }
 }
+
+const Parent = React.createClass({
+  render () {
+    // let items = this.props.children.map(child => child) //throws an error for single child
+    // let items = React.Children.map(this.props.children, child => child) //solution 1
+    // let items = React.Children.toArray(this.props.children) //solution 2
+    let items = React.Children.forEach(this.props.children, child => console.log(child.props.className)) //solution 3
+    // console.log(items);
+    return null
+  }
+})
+
 
 export default App
